8ab245294a352a0a49603a30547d250cad6e5cd1
Saif <saifahmadsaif@gmail.com>
2014-12-04 18:07:46 +0530

    project

--- plot1.R
+++ plot1.R
@@ -10,3 +10,5 @@ globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
+
+
